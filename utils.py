fbf349edefbf364cb1ea10f3cf03e0fd0d659661
Julius Alphonso <juliusalphonso.09@gmail.com>
2022-12-07 17:48:38 +0000

    Day 5: Supply Stacks

--- utils.py
+++ utils.py
@@ -1,4 +1,15 @@
-from typing import Callable, TypeVar
+import re
 
-TInput = TypeVar("TInput")
-TOutput = TypeVar("TOutput")
+
+def chunkify(arr: list, chunk_size: int):
+  for i in range(0, len(arr), chunk_size):
+    yield arr[i:i + chunk_size]
+
+
+def filter_alphnumeric(s: str):
+  s = re.sub('[^a-zA-Z]', '', s)
+  return s.strip()
+
+
+def not_empty(s: str):
+  return s != ''
